62a4d18a9007652cd163c973364bb2c03dc39d8b
xup3rr <xup3rr@gmail.com>
2023-10-17 01:31:53 +0200

    ✨ Add RandomTodo button
    Fetch from jsonplaceholder public api

--- src/components/TodoAdd.tsx
+++ src/components/TodoAdd.tsx
@@ -1,11 +1,12 @@
-import { useAtom } from "jotai";
-import { PlusIcon } from "./Icons";
-import { NewTodoAtom } from "../store/TodoStore";
 import clsx from "clsx";
+import { useAtom } from "jotai";
 import { useEffect } from "react";
+import { NewRandomTodoAtom, NewTodoAtom } from "../store/TodoStore";
+import { CloudIcon, PlusIcon, StarIcon } from "./Icons";
 
 const TodoAdd: React.FC = () => {
   const [todos, addTodo] = useAtom(NewTodoAtom);
+  const [loading, addRandomTodo] = useAtom(NewRandomTodoAtom);
 
   const disabled = todos[todos.length - 1].task.length === 0;
 
@@ -20,10 +21,10 @@ const TodoAdd: React.FC = () => {
     return () => {
       window.removeEventListener("keypress", keyPressHandler);
     };
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+  }, [todos]); // eslint-disable-line react-hooks/exhaustive-deps
 
   const addNewTodo = () => {
-    if (todos[todos.length - 1].task.length > 0) {
+    if (!disabled) {
       addTodo({
         id: crypto.randomUUID(),
         task: "",
@@ -33,13 +34,22 @@ const TodoAdd: React.FC = () => {
   };
 
   return (
-    <button
-      className={clsx({ "opacity-50": disabled })}
-      onClick={addNewTodo}
-      disabled={disabled}
-    >
-      <PlusIcon />
-    </button>
+    <div className="flex gap-x-2 py-2 sticky bottom-0">
+      <button
+        className={clsx({ "opacity-50": disabled })}
+        onClick={addNewTodo}
+        disabled={disabled}
+      >
+        <PlusIcon />
+      </button>
+      <button
+        className={clsx({ "opacity-50": disabled })}
+        onClick={addRandomTodo}
+        disabled={disabled}
+      >
+        {loading ? <CloudIcon /> : <StarIcon />}
+      </button>
+    </div>
   );
 };
 
--- src/store/TodoStore.ts
+++ src/store/TodoStore.ts
@@ -24,4 +24,39 @@ const NewTodoAtom = atom(
   }
 );
 
+const loadingAtom = atom<boolean>(false);
+// Add a random todo
+export const NewRandomTodoAtom = atom(
+  (get) => get(loadingAtom),
+  async (get, set) => {
+    set(loadingAtom, true);
+    try {
+      //TODO: get random todo from chatgpt
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/todos/${Math.floor(
+          Math.random() * 200
+        )}`
+      );
+
+      const { title } = await response.json();
+      const newTodo: Todo = {
+        id: crypto.randomUUID(),
+        task: title,
+        done: false,
+      };
+      set(Todos, [...get(Todos), newTodo]);
+      set(loadingAtom, false);
+    } catch (e) {
+      console.error(e);
+      const newTodo: Todo = {
+        id: crypto.randomUUID(),
+        task: "Failed to fetch todo",
+        done: true,
+      };
+      set(Todos, [...get(Todos), newTodo]);
+      set(loadingAtom, false);
+    }
+  }
+);
+
 export { TodosAtoms, NewTodoAtom };
